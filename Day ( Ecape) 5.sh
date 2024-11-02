dff76ab31be757274f809369e59d570794997a94
prateekmudgal <mudgalprateek00@gmail.com>
2024-11-02 10:03:32 +0530

    for my escape

---
+++ Day ( Ecape) 5.sh
@@ -0,0 +1,11 @@
+#here it take as the one command though print in this vertical way and \ treat as abhi command baaki h  
+echo " my \
+name \
+is \
+gaurav \
+"
+
+echo -e "this is gaurav \t saurav \t test name"  #it will escape as tab
+echo -e "this is gaurav \v saurav \v test name" # it will escape in vertical way
+
+echo -e "this is gaurav \n saurav \n test name"  # in thi escape as new line
