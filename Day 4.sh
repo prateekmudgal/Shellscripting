57503dccb7c53753b93b6850820ab6d8e365b635
prateekmudgal <mudgalprateek00@gmail.com>
2024-11-02 09:56:13 +0530

    for my day 4

---
+++ Day 4.sh
@@ -0,0 +1,15 @@
+#single line commmand
+
+#!/bin/bash
+# this is a comment.
+
+#for mutliple line command
+#!/bin/bash
+: '
+This is
+multiline comment
+in shellscript
+'
+
+
+#Use : ' to open and ' to close (make sure you have space between : and ')
